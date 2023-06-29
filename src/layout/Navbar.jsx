f5fcc6e90d55a87cc702c4ca26ee81f9fb56b66b
nandan <devadula.nandan@gmail.com>
2023-06-29 21:54:30 +0530

    progress++

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -93,7 +93,7 @@ export default function Navbar() {
                                         <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
                                     </svg>
                                 </button></li>
-                                <li>
+                                <li onClick={() => { document.getElementById("my-drawer").checked = false }}>
                                     <a href="#home" className="text-accent font-extrabold">HOME</a>
                                     <a href="#about" className=" font-extrabold">ABOUT</a>
                                     <a href="#skills" className=" font-extrabold">SKILLS</a>
