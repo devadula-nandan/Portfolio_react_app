430387c029864582a05c66c672f3de9222628408
nandan <devadula.nandan@gmail.com>
2023-07-03 13:55:18 +0530

    persistence

--- api/counter.js
+++ api/counter.js
@@ -1,13 +1,22 @@
+const fs = require('fs');
+
 let count = 0;
+
+const incrementCount = () => {
+  count++;
+  fs.writeFileSync('count.txt', count.toString());
+};
 
 const handler = (req, res) => {
   if (req.method === 'GET') {
-    ++count;
+    incrementCount();
+    const storedCount = fs.readFileSync('count.txt', 'utf-8');
+    count = parseInt(storedCount, 10);
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.setHeader('Access-Control-Allow-Methods', 'GET');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.status(200).json({ count });
-  }  else {
+  } else {
     res.setHeader('Allow', 'GET');
     res.status(405).end();
   }
